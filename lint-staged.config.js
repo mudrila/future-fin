d81a52c761fe634df8cdc01797ed022d420be9a0
Kirill Klimenko <klimenkomud@gmail.com>
2020-08-07 11:01:13 +0300

    Update lint-staged config to exclude .next folder

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "{,!(node_modules)/**/}!(.storybook)/*.{js,jsx}": [
+  "{,!(node_modules)/**/}!(.storybook)!(.next)/*.{js,jsx}": [
     "node_modules/.bin/prettier --write",
     "eslint --fix --max-warnings=0",
     "git add"
