0b9d3bbfd53b48d7f0690d4491640148d637a02f
Kirill Klimenko <klimenkomud@gmail.com>
2020-09-22 17:54:45 +0300

    Expose API_URL to browser

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 console.log("ENV VARS", process.env, process.env.API_URL);
 
 export default {
-  API_URL: process.env.API_URL || "http://localhost:8080",
+  API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
   CRUD_MAP_TO_METHODS: {
     CREATE: "post",
     READ: "get",
